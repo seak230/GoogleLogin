736ab5b8c5fafc80f91d72e6177f487c1f18a3d7
seak23 <113758643+seak230@users.noreply.github.com>
2024-03-24 16:57:59 +0900

    Add: Design Sign&Login Page

--- app/src/main/java/com/nohjason/logintest/navigation/NavControll.kt
+++ app/src/main/java/com/nohjason/logintest/navigation/NavControll.kt
@@ -1,9 +1,21 @@
 package com.nohjason.logintest.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.nohjason.logintest.R
+import com.nohjason.logintest.rememberFirebaseAuthLauncher
 import com.nohjason.logintest.screen.Login
 import com.nohjason.logintest.screen.Profile
 
@@ -11,11 +23,40 @@ import com.nohjason.logintest.screen.Profile
 fun NavControll() {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "profile") {
-        composable("login") { Login() }
-        composable("profile") {
-            Profile(navController = navController, value = "")
+    val auth = Firebase.auth
+    var user by remember { mutableStateOf(auth.currentUser) }
+    val launcher = rememberFirebaseAuthLauncher(
+        onAuthComplete = { result ->
+            user = result.user
+        },
+        onAuthError = {
+            user = null
         }
-        // Add more destinations similarly.
+    )
+    val token = stringResource(R.string.default_web_client_id)
+    val context = LocalContext.current
+    val gso =
+        GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(token)
+            .requestEmail()
+            .build()
+    val googleSignInClient = GoogleSignIn.getClient(context, gso)
+
+    NavHost(navController = navController, startDestination = "login") {
+        composable("login") {
+            Login(
+                navController,
+                googleSignInClient,
+                user,
+                launcher
+            )
+        }
+        composable("profile") {
+            Profile(
+                navController = navController,
+                user,
+                googleSignInClient
+            )
+        }
     }
 }
--- app/src/main/java/com/nohjason/logintest/screen/Profile.kt
+++ app/src/main/java/com/nohjason/logintest/screen/Profile.kt
@@ -1,13 +1,58 @@
 package com.nohjason.logintest.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.firebase.auth.FirebaseUser
 
 @Composable
-fun Profile (navController: NavController, value: String?) {
-    Column {
-        Text(text = "$value")
+fun Profile (navController: NavController, user: FirebaseUser?, googleSignInClient: GoogleSignInClient) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Box {
+            AsyncImage(
+                model = "${user!!.photoUrl}",
+                contentDescription = null,
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(200.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Text(
+            text = " ${user!!.displayName}",
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp
+        )
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Button(onClick = {
+            googleSignInClient.signOut()
+            navController.navigate("login")
+        }) {
+            Text("Sign out")
+        }
     }
 }
